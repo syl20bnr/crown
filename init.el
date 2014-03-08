4d97f82ad70736bf2e31114db95d4664393f32c2
syl20bnr <sylvain.benner@gmail.com>
2014-03-08 01:27:06 -0500

    update function

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 ;; `package' has to be initialized to install pkgs
 (package-initialize)
 
-(defun coco-install ()
+(defun croco-install-croco ()
   "Install or update the prerequisites for Coco.
 
 Prerequisites list:
@@ -11,7 +11,9 @@ Prerequisites list:
   (let ((temp-dir (make-temp-file "coco" t)))
     ;; if we do not have melpa's package-build,
     ;; get the tar package and install it
-    (unless (package-installed-p 'package-build)
+    (if (package-installed-p 'package-build)
+        ;; update the package if new version found
+        ()
       (let* ((archive "http://melpa.milkbox.net/packages/")
              (archive-contents (with-temp-buffer
                                  (url-insert-file-contents
@@ -29,3 +31,34 @@ Prerequisites list:
         (url-copy-file archive-url file t)
         (package-install-file file)))
     (delete-directory temp-dir t)))
+
+(defun croco-get-melpa-package-version (package)
+  (let* ((temp-dir (make-temp-file "croco" t))
+         (archive "http://melpa.milkbox.net/packages/")
+         (archive-contents (with-temp-buffer
+                             (url-insert-file-contents
+                              (concat archive "archive-contents"))
+                             (cdr (read (current-buffer)))))
+         (pkg-info (cdr (assoc package archive-contents))))
+    (unless (not pkg-info)
+      (package-version-join (aref pkg-info 0)))))
+
+(defun croco-get-inst-package-version (package) "12235")
+(defun croco-get-inst-package-version (package)
+  (let ((pkg-info (cdr (assoc package package-alist))))
+    (unless (not pkg-info)
+      (package-version-join (aref pkg-info 0)))))
+
+(defun croco-update-croco ()
+  (let ((pb-melpa-version (croco-get-melpa-package-version 'package-build))
+        (pb-inst-version (croco-get-inst-package-version 'package-build)))
+    (if (and pb-inst-version (equal pb-inst-version pb-melpa-version))
+        (message "croco package build dependency already up to date")
+      (message "croco package build dependency needs to be updated"))))
+
+(message "%s" package-alist)
+(croco-install-croco)
+(croco-get-inst-package-version 'package-build)
+(croco-get-melpa-package-version 'package-build)
+(croco-update-croco)
+
